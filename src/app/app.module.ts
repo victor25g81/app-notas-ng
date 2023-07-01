22e4b6f8768f0914cdf288f41ff36ef9330d8e1e
victor25g81 <100184175+victor25g81@users.noreply.github.com>
2023-07-01 15:52:38 -0600

    Componente título e instalación Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TituloComponent
   ],
   imports: [
     BrowserModule
